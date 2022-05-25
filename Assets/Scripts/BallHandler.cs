3fd44253a281527f6824b03873f282f8c63151f7
Russell Oyama <russelloyama@gmail.com>
2022-05-25 08:13:27 -1000

    detaching string after ball launch

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -6,7 +6,12 @@ using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
 
+    [SerializeField] private Rigidbody2D currentBallRigidBody;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    [SerializeField] private float detachDelay;
+
     private Camera mainCamera;
+    private bool isDragging;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +22,36 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(currentBallRigidBody == null) { return; }
+
         if (!Touchscreen.current.primaryTouch.press.isPressed)
         {
+            if (isDragging)
+            {
+                LaunchBall();
+            }
+            isDragging = false;
             return;
         }
 
+        isDragging = true;
+        currentBallRigidBody.isKinematic = true;
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+        currentBallRigidBody.position = worldPosition;
+    }
 
-        Debug.Log(worldPosition);
+    private void LaunchBall()
+    {
+        currentBallRigidBody.isKinematic = false;
+        currentBallRigidBody = null;
+
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
